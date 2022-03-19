9dd33c2d9fca5ba4a69562449f12c0c4a1a7f993
noflm <61629565+noflm@users.noreply.github.com>
2022-03-20 01:13:25 +0900

    add code

--- cogs/api_tasks.py
+++ cogs/api_tasks.py
@@ -1,35 +1,50 @@
 import discord
 import asyncio
 import aiohttp
+import json
 import os
 import time
 from discord.ext import tasks, commands
+from logging import getLogger, config
+from main import log_config
 
-api_base = os.environ.get("API_URL")
+config.dictConfig(log_config)
 
 
 class APITasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.logger = getLogger('api_tasks')
+        self.api_base = os.environ.get("API_URL")
+        self.youtube_data = {}
         self.api_tasks.start()
 
     def cog_unload(self):
         self.api_tasks.cancel()
         
+
     @tasks.loop(seconds=5)
-    async def api_tasks(self):
+    async def youtube_data(self):
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{api_base}/youtube/") as resp:
+            async with session.get(f"{self.api_base}/youtube/") as resp:
                 if resp.status != 200:
+                    self.logger.error("API Contact Error!!")
                     return
+                self.youtube_data = await resp.json()
+                
+    
+        
+            
+            
+            
                 
                 
         
         
     @api_tasks.before_loop
     async def wait_api_tasks(self):
-        print('tasks start waiting...')
         await self.bot.wait_until_ready()
+        self.logger.info('api tasks start waiting...')
         
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 import discord
+import json
 import os
 import time
 import aiohttp
+from dotenv import load_dotenv
+from os.path import join, dirname
+from logging import getLogger, config
 
-try:
-    from dotenv import load_dotenv
-    from os.path import join, dirname
+with open('./log_config.json', 'r') as f:
+    log_config = json.load(f)
+config.dictConfig(log_config)
+logger = getLogger('main')
 
-    load_dotenv(verbose=True)
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-except:
-    pass
-
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 bot = discord.Bot()
 
 #Load extensions
-#bot.load_extension('cogs.api_tasks')
+bot.load_extension('cogs.api_tasks')
 
 
 @bot.event
 async def on_ready():
     # Bot起動時にターミナルに通知を出す
-    print('-'*20)
-    print('ログインしました')
-    print('-'*20)
+    logger.info('-'*30)
+    logger.info('EliteBotが起動しました')
+    logger.info('-'*30)
     timestamp = int(time.time())
     bot_chid = int(os.environ.get("BOT_CHID"))
-    await bot.change_presence(activity=discord.Game(name='Nyahello!!'))
+    await bot.change_presence(activity=discord.Game(name="Nyahello!! I'm Elite Nye!"))
     bot_ch = bot.get_channel(bot_chid)
-    await bot_ch.send(f"<t:{timestamp}:F>\nEliteBotが起動しました")
-    check_api = await bot_ch.send("MikoAPIへの接続を確認中...")
+    #await bot_ch.send(f"<t:{timestamp}:F>\nEliteBotが起動しました")
+    #check_api = await bot_ch.send("MikoAPIへの接続を確認中...")
+    logger.info("MikoAPIへの接続を確認中...")
     async with aiohttp.ClientSession() as session:
         async with session.get(os.environ.get("API_URL")) as res:
             if res.status == 200:
-                await check_api.edit("MikoAPIへの接続に成功しました")
+                #await check_api.edit("MikoAPIへの接続に成功しました")
+                logger.info("MikoAPIへの接続に成功しました")
             else:
-                await check_api.edit("MikoAPIへの接続に失敗しました")
+                #await check_api.edit("MikoAPIへの接続に失敗しました")
+                logger.error("MikoAPIへの接続に失敗しました")
 
 
 if __name__ == "__main__":
