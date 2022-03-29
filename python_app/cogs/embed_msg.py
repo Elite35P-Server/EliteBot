184b9ed4089f91a0ed0461eaad7aaabf487b2368
noflm <61629565+noflm@users.noreply.github.com>
2022-03-30 02:05:56 +0900

    Update embed_msg.py

--- python_app/cogs/embed_msg.py
+++ python_app/cogs/embed_msg.py
@@ -151,7 +151,7 @@ def ytvideo_notice_like_live(id: str, name: str, icon: str, title: str, url: str
 
 # Twitch channel
 def tcch_notice_name(display_id: str, old_name: str, new_name: str, icon: str):
-    embed=discord.Embed(title=f"{new_name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description="**Twitchのチャンネル名が変更されました**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=f"{new_name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description="**Twitchのチャンネル名が変更されました**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_image(url=icon)
     embed.add_field(name="変更前", value=f"**{old_name}**", inline=True)
     embed.add_field(name="変更後", value=f"**{new_name}**", inline=True)
@@ -159,7 +159,7 @@ def tcch_notice_name(display_id: str, old_name: str, new_name: str, icon: str):
     return embed
 
 def tcch_notice_displayid(name: str, old_displayid: str, new_displayid: str, icon: str):
-    embed=discord.Embed(f"{name}({new_displayid})", url=f"https://www.twitch.tv/{new_displayid}", description="**Twitchのチャンネル表示IDが変更されました**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(f"{name}({new_displayid})", url=f"https://www.twitch.tv/{new_displayid}", description="**Twitchのチャンネル表示IDが変更されました**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_image(url=icon)
     embed.add_field(name="変更前", value=f"**{old_displayid}**", inline=True)
     embed.add_field(name="変更後", value=f"**{new_displayid}**", inline=True)
@@ -167,7 +167,7 @@ def tcch_notice_displayid(name: str, old_displayid: str, new_displayid: str, ico
     return embed
 
 def tcch_notice_subsc(display_id: str, name: str, icon: str, subsc: int, updated_at: DateTime):
-    embed=discord.Embed(title=f"{name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description=f"**Twitchのチャンネルフォロワー数が{subsc/10000:.0f}万人を突破しました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=f"{name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description=f"**Twitchのチャンネルフォロワー数が{subsc/10000:.0f}万人を突破しました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_image(url=icon)
     embed.add_field(name="達成日時", value=f"**<t:{int(updated_at.timestamp())}:F>(<t:{int(updated_at.timestamp())}:R>)**", inline=True)
     embed.add_field(name="現在のフォロワー数", value=f"**{subsc:,}人**", inline=True)
@@ -175,7 +175,7 @@ def tcch_notice_subsc(display_id: str, name: str, icon: str, subsc: int, updated
     return embed
 
 def tcch_notice_play(display_id: str, name: str, icon: str, play_count: int, updated_at: DateTime):
-    embed=discord.Embed(title=f"{name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description=f"**Twitchのチャンネル総再生回数が{play_count/10000:.0f}万回を突破しました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=f"{name}({display_id})", url=f"https://www.twitch.tv/{display_id}", description=f"**Twitchのチャンネル総再生回数が{play_count/10000:.0f}万回を突破しました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_image(url=icon)
     embed.add_field(name="達成日時", value=f"**<t:{int(updated_at.timestamp())}:F>(<t:{int(updated_at.timestamp())}:R>)**", inline=True)
     embed.add_field(name="現在の総再生回数", value=f"**{play_count:,}回**", inline=True)
@@ -184,7 +184,7 @@ def tcch_notice_play(display_id: str, name: str, icon: str, play_count: int, upd
 
 # Twitch stream
 def tcstream_notice_title(display_id: str, name: str, icon: str, url: str, thumbnail: str, old_title: str, new_title: str):
-    embed=discord.Embed(title=new_title, url=url, description="**動画/配信タイトルが変更されました**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=new_title, url=url, description="**動画/配信タイトルが変更されました**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="変更前", value=f"**{old_title}**", inline=True)
@@ -193,7 +193,7 @@ def tcstream_notice_title(display_id: str, name: str, icon: str, url: str, thumb
     return embed
 
 def tcstream_notice_nonetolive(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, astime: DateTime):
-    embed=discord.Embed(title=title, url=url, description="**配信が開始されました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description="**配信が開始されました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="開始日時", value=f"**<t:{int(astime.timestamp())}:F>(<t:{int(astime.timestamp())}:R>)**", inline=True)
@@ -201,7 +201,7 @@ def tcstream_notice_nonetolive(display_id: str, name: str, icon: str, title: str
     return embed
 
 def tcstream_notice_livetonone(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, astime: DateTime, aetime: DateTime):
-    embed=discord.Embed(title=title, url=url, description="**配信が終了しました**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description="**配信が終了しました**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="開始日時", value=f"**<t:{int(astime.timestamp())}:F>(<t:{int(astime.timestamp())}:R>)**", inline=True)
@@ -210,7 +210,7 @@ def tcstream_notice_livetonone(display_id: str, name: str, icon: str, title: str
     return embed
 
 def tcstream_notice_upload(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, createdat: DateTime):
-    embed=discord.Embed(title=title, url=url, description="**動画が投稿されました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description="**動画が投稿されました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="投稿日時", value=f"**<t:{int(createdat.timestamp())}:F>(<t:{int(createdat.timestamp())}:R>)**", inline=True)
@@ -218,7 +218,7 @@ def tcstream_notice_upload(display_id: str, name: str, icon: str, title: str, ur
     return embed
 
 def tcstream_notice_currentviewers(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, trigger: int, current_viewers: int, updated_at: DateTime):
-    embed=discord.Embed(title=title, url=url, description=f"**現在行われている配信の同時接続数(視聴者数)が{trigger/10000}万人を突破しました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description=f"**現在行われている配信の同時接続数(視聴者数)が{trigger/10000}万人を突破しました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="達成日時", value=f"**<t:{int(updated_at.timestamp())}:F>(<t:{int(updated_at.timestamp())}:R>)**", inline=True)
@@ -227,7 +227,7 @@ def tcstream_notice_currentviewers(display_id: str, name: str, icon: str, title:
     return embed
 
 def tcstream_notice_play(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, trigger: int, view_count: int, updated_at: DateTime):
-    embed=discord.Embed(title=title, url=url, description=f"**再生回数が{trigger:,}回を突破しました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description=f"**再生回数が{trigger:,}回を突破しました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="達成日時", value=f"**<t:{int(updated_at.timestamp())}:F>(<t:{int(updated_at.timestamp())}:R>)**", inline=True)
@@ -236,7 +236,7 @@ def tcstream_notice_play(display_id: str, name: str, icon: str, title: str, url:
     return embed
 
 def tcstream_notice_play_live(display_id: str, name: str, icon: str, title: str, url: str, thumbnail: str, trigger: int, view_count: int, updated_at: DateTime):
-    embed=discord.Embed(title=title, url=url, description=f"**現在行われている配信の再生回数が{trigger:,}回を突破しました!**", color=0xff0000, timestamp=dt.utcnow())
+    embed=discord.Embed(title=title, url=url, description=f"**現在行われている配信の再生回数が{trigger:,}回を突破しました!**", color=0x864ffe, timestamp=dt.utcnow())
     embed.set_author(name=name, url=f"https://www.twitch.tv/{display_id}", icon_url=icon)
     embed.set_image(url=thumbnail)
     embed.add_field(name="達成日時", value=f"**<t:{int(updated_at.timestamp())}:F>(<t:{int(updated_at.timestamp())}:R>)**", inline=True)
