325ed40d20bb233a1836a023215ab7016daf6701
noflm <takaashi1024@gmail.com>
2022-06-03 21:30:20 +0900

    fix

--- python_app/main.py
+++ python_app/main.py
@@ -13,11 +13,6 @@ logger = getLogger('main')
 
 bot = discord.Bot()
 
-#Load extensions
-bot.load_extension('cogs.api_tasks')
-bot.load_extension('cogs.notification')
-bot.load_extension('cogs.commands')
-
 @bot.event
 async def on_ready():
     await db.setup()
@@ -27,19 +22,24 @@ async def on_ready():
     logger.info('-'*30)
     timestamp = int(time.time())
     bot_chid = int(os.environ.get('BOT_CHID'))
-    await bot.change_presence(activity=discord.Game(name="Nyahello!! I'm Elite Nye!"))
+    await bot.change_presence(activity=discord.Game(name="にゃっはろ～!! みこちはえりーとだにぇ!!"))
     bot_ch = bot.get_channel(bot_chid)
-    #await bot_ch.send(f"<t:{timestamp}:F>\nEliteBotが起動しました")
-    #check_api = await bot_ch.send("MikoAPIへの接続を確認中...")
+    await bot_ch.send(f"<t:{timestamp}:F>\nEliteBotが起動しました")
+    check_api = await bot_ch.send("MikoAPIへの接続を確認中...")
     logger.info('Checking API connection...')
     async with aiohttp.ClientSession() as session:
         async with session.get(os.environ.get('API_URL')) as resp:
             if resp.status == 200:
-                #await check_api.edit("MikoAPIへの接続に成功しました")
+                await check_api.edit("MikoAPIへの接続に成功しました")
                 logger.info('API connection success.')
             else:
-                #await check_api.edit("MikoAPIへの接続に失敗しました")
+                await check_api.edit(f"MikoAPIへの接続に失敗しました ({resp.status})")
                 logger.error('API connection failed.', resp.text)
+                
+    bot.load_extension('cogs.api_tasks')
+    bot.load_extension('cogs.notification')
+    bot.load_extension('cogs.commands')
+
 
 
 if __name__ == '__main__':
